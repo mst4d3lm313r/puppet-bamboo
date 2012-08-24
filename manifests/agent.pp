0e40770491a2f34802d6a3a6b479631085c999d1
Justin Stoller <justin@puppetlabs.com>
2012-08-24 15:22:28 -0700

    initial commit

---
+++ manifests/agent.pp
@@ -0,0 +1,122 @@
+#
+# Usage:
+# class { 'bamboo::client':
+#   server      => 'builder-master.acctest.dc1.puppetlabs.net',
+#                                    # ^^ fqdn of bamboo master, no default
+#   server_port => '8085'            # port of bamboo master, default shown
+#   version     => '4.2.0',          # the version is needed, default shown
+#   agent_home  => '/var/lib/bamboo' # make sure all parent directories exist, default shown
+#   user        => 'bamboo-agent'    # user to run agent as, default shown
+#   group       => 'bamboo-agent'    # group to install as, default shown
+# }
+class bamboo::agent (
+
+  $server_port = '8085',
+  $version     = '4.2.0',
+  $user        = 'bamboo-agent',
+  $group       = 'bamboo-agent',
+  $home        = "undefined",
+  $server
+
+) {
+
+  $agent_jar = "atlassian-bamboo-agent-installer-${version}.jar"
+  $agent_home = $home ? {
+    'undefined' => "/var/lib/${user}",
+     default    => $home
+  }
+
+  group { $group:
+    ensure => present,
+  }
+
+  user { $user:
+    home    => $agent_home,
+    ensure  => present,
+    gid     => $group,
+    require => Group[$group],
+  }
+
+  file { $agent_home:
+    ensure  => directory,
+    owner   => $user,
+    require => User[$user],
+  }
+
+  exec { 'download-bamboo-agent-jar':
+    command  => "wget http://${server}:${server_port}/agentServer/agentInstaller/${agent_jar}",
+    cwd      => $agent_home,
+    path     => ['/usr/bin', '/bin'],
+    creates  => "${agent_home}/atlassian-agent-installer-${version}.jar",
+    require  => File[$agent_home],
+  }
+
+  file { "${agent_home}/${agent_jar}":
+    ensure  => present,
+    owner   => $user,
+    require => Exec['download-bamboo-agent-jar'],
+  }
+
+  exec { 'install-bamboo-agent':
+    command => "java -jar -Dbamboo.home=${agent_home} ${agent_jar} http://${server} install",
+    cwd     => $agent_home,
+    path    => [ '/bin', '/usr/bin', '/usr/local/bin' ],
+    creates => "${agent_home}/bin",
+    require => File[ "${agent_home}/${agent_jar}" ],
+  }
+
+  if ! defined(File['/var/lock/subsys']) {
+    file { '/var/lock/subsys': ensure => directory, owner => root }
+  }
+
+  file { "/var/lock/subsys/${user}":
+    ensure => directory,
+    owner  => $user,
+  }
+
+  file { "/var/run/${user}":
+    ensure => directory,
+    owner  => $user,
+  }
+
+  file { "/etc/init.d/${user}":
+    ensure  => link,
+    target  => "${agent_home}/bin/bamboo-agent.sh",
+    require => Exec[ 'install-bamboo-agent' ],
+  }
+
+  File_line { path => "${agent_home}/bin/bamboo-agent.sh",
+    require => Exec['install-bamboo-agent'],
+    before  => Service['bamboo-agent'],
+  }
+
+  file_line { 'bamboo-initscript-wrapper-cmd':
+    line  => "WRAPPER_CMD='${agent_home}/bin/wrapper'",   
+    match => '^WRAPPER_CMD=',
+  }
+
+  file_line { 'bamboo-initscript-wrapper-conf':
+    line  => "WRAPPER_CONF='${agent_home}/conf/wrapper.conf'",   
+    match => '^WRAPPER_CONF=',
+  }
+
+  file_line { 'bamboo-initscript-lockdir':
+    line  => "LOCKDIR='/var/lock/subsys/${user}'",   
+    match => '^LOCKDIR=',
+  }
+
+  file_line { 'bamboo-initscript-piddir':
+    line  => "PIDDIR='/var/run/${user}'",   
+    match => '^PIDDIR=',
+  }
+
+  file_line { 'bamboo-initscript-run-user':
+    line => "RUN_AS_USER='${user}'",
+    match => '^\S*RUN_AS_USER=',
+  }
+
+  service { 'bamboo-agent':
+    ensure => 'running',
+    enable => true,
+  }
+}
